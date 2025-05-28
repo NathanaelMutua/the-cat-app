9483d7e25f7d688e8fb43bc3ebaa1dcb5b7555c9
Nxsh <nathanael.mutua.m@gmail.com>
2025-05-29 00:07:34 +0300

    add fact logic | add spinner | update styling
    - add function 'getDataFacts()' to extract facts from data array and
    into HTML list-tag format
    - change let variables to const
    - add 'displayedFactsHolder' DOM to display get results
    - add spinner, with relative styling
    - add functions to activate and deactivate the spinner
    - display list using innerHTML
    - update styling

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
-let factCount = document.getElementById("cat-facts-count");
-let photoCount = document.getElementById("cat-photos-count");
-let photoBtn = document.getElementById("photo-submit-btn");
-let factBtn = document.getElementById("fact-submit-btn");
+const factCount = document.getElementById("cat-facts-count");
+const photoCount = document.getElementById("cat-photos-count");
+const photoBtn = document.getElementById("photo-submit-btn");
+const factBtn = document.getElementById("fact-submit-btn");
+const displayedFactsHolder = document.querySelector(".result-content-holder");
+const spinner = document.getElementById("spinner");
+
+
+// assigning data values to an ordered list that we can output
+let getDataFacts = function(array){
+    let factListItems = ""; // this will hold the list items
+    for (let i = 0; i < array.length; i++){
+        factListItems += `<li>${array[i]}</li>`;// this adds the values one after the other to the list as list items
+    }
+    const factList = `<ol>${factListItems}</ol>`;
+    return factList
+}
+
+// activating the spinner
+function startSpinner(){
+    spinner.style.display = "block";
+}
+
+// deactivation the spinner
+function stopSpinner(){
+    spinner.style.display = "none";
+}
 
 
 factBtn.addEventListener("click", async function getFact(){
+    displayedFactsHolder.innerHTML = null;
+    startSpinner()
+
     let factCountValue = Number(factCount.value);
     let maxfactValue = 50;
 
@@ -12,13 +38,17 @@ factBtn.addEventListener("click", async function getFact(){
     if (factCountValue > 50){
         factCountValue = maxfactValue;
     } else if (factCountValue === 0){
-        factCountValue = 1;// I realiesd for 0 values the output always has atleast one fact
+        factCountValue = 1; // I realiesd for 0 values the output always has atleast one fact
     }
 
     try{
         const response = await axios.get(`https://meowfacts.herokuapp.com/?count=${factCountValue}`)
-        const data = response.data;
-        console.log(data);
+        const data = response.data.data; // this gives us the array
+        
+        stopSpinner()
+
+        displayedFactsHolder.innerHTML = getDataFacts(data);
+        
 
     } catch (error){
         console.error();
